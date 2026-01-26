f979bbc840889dcff8519c1bc256a08bf3812acb
RAZEPAL <s2696596472@163.com>
2026-01-26 09:07:12 +0800

    注释无用函数buildlinkpairsfrom

--- buildLinkPairsFromSequence.m
+++ buildLinkPairsFromSequence.m
@@ -1,18 +1,18 @@
-function pairs = buildLinkPairsFromSequence(seq, nameMap)
-% seq: 例如 "213"
-% nameMap: containers.Map('1'->'satellite1', ...)
-
-pairs = struct('satA',{},'satB',{});
-
-for i = 1:length(seq)-1
-    a = seq(i);
-    b = seq(i+1);
-
-    if ~isKey(nameMap,a) || ~isKey(nameMap,b)
-        continue
-    end
-
-    pairs(end+1).satA = nameMap(a);
-    pairs(end).satB   = nameMap(b);
-end
-end
+% function pairs = buildLinkPairsFromSequence(seq, nameMap)
+% % seq: 例如 "213"
+% % nameMap: containers.Map('1'->'satellite1', ...)
+% 
+% pairs = struct('satA',{},'satB',{});
+% 
+% for i = 1:length(seq)-1
+%     a = seq(i);
+%     b = seq(i+1);
+% 
+%     if ~isKey(nameMap,a) || ~isKey(nameMap,b)
+%         continue
+%     end
+% 
+%     pairs(end+1).satA = nameMap(a);
+%     pairs(end).satB   = nameMap(b);
+% end
+% end
